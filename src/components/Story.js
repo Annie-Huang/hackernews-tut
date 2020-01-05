37113d09335baca1f1d95f0749d478f563ea55aa
Annie Huang <annie.hn@gmail.com>
2020-01-05 17:41:09 +1100

    Confirmed we can displays everything on Story.js

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from 'react';
 import {getStory} from '../services/hnApi';
 
 export const Story = ({storyId}) => {
+    const [story, setStory] = useState({});
+
     useEffect(() => {
-        getStory(storyId).then(data => console.log(data));
+        getStory(storyId).then(data => data && data.url && setStory(data));
     }, []);
 
-    return <p>I am a story! {storyId}</p>;
+    return <p>{JSON.stringify(story)}</p>;
 };
