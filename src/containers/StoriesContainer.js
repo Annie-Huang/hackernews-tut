d15b2fd59c2d7f59bc1d8f678dbaaac0a9b7cfd7
Annie Huang <annie.hn@gmail.com>
2020-01-05 21:07:45 +1100

    Change React.Fragment to <StoryWrapper>

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {getStoryIds, getStory} from '../services/hnApi';
 import {Story} from "../components/Story";
+import {StoryWrapper} from "../styles/StoryStyles";
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
@@ -10,12 +11,12 @@ export const StoriesContainer = () => {
     }, []);
 
     return (
-        <>
+        <StoryWrapper data-testid="story">
             <h1>Hacker News Stories</h1>
             {storyIds.map(storyId => (
                 <Story key={storyId} storyId={storyId}/>
             ))}
-        </>
+        </StoryWrapper>
     );
 
 };
