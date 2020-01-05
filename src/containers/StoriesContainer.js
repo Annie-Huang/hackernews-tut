5735881bd55beffc6c20c5e36c109eeb7848be98
Annie Huang <annie.hn@gmail.com>
2020-01-05 17:24:40 +1100

    Create another api call of getStory in hnApi.js and test it in StoriesContainer.js

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {getStoryIds} from '../services/hnApi';
+import {getStoryIds, getStory} from '../services/hnApi';
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
 
     useEffect(() => {
         getStoryIds().then(data => setStoryIds(data));
+        getStory(20970623).then(data => console.log(data));
     }, []);
 
     return (
--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -5,6 +5,12 @@ export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
+export const getStory = async (storyId) => {
+    // Or you can do ${storyUrl+storyId}.json
+    const result = await axios.get(`${storyUrl}${storyId}.json`).then(({data}) => data);
+    return result;
+};
+
 export const getStoryIds = async () => {
     const result = await axios.get(newStoriesUrl).then(({data}) => data);
     return result;
