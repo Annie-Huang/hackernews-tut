9642b496ecf59e8824e491b2738e80b45f1f939f
Annie Huang <annie.hn@gmail.com>
2020-01-05 21:18:55 +1100

    Do the StoryWrapper on the Story.js, not StoriesContainer.js

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {getStoryIds, getStory} from '../services/hnApi';
 import {Story} from "../components/Story";
-import {StoryWrapper} from "../styles/StoryStyles";
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
@@ -11,12 +10,12 @@ export const StoriesContainer = () => {
     }, []);
 
     return (
-        <StoryWrapper data-testid="story">
+        <>
             <h1>Hacker News Stories</h1>
             {storyIds.map(storyId => (
                 <Story key={storyId} storyId={storyId}/>
             ))}
-        </StoryWrapper>
+        </>
     );
 
 };
