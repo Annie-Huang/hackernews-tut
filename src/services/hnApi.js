dae037b573492831c8a9ef7c98c2af46a0406034
Annie Huang <annie.hn@gmail.com>
2020-01-08 21:31:34 +1100

    Mirror the file the same as the author's github. But still scroll does not fetch more data, not sure why...

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -7,14 +7,22 @@ export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
 export const getStory = async (storyId) => {
-    // Or you can do ${storyUrl+storyId}.json
+    // // Or you can do ${storyUrl+storyId}.json
+    // const result = await axios
+    //     .get(`${storyUrl}${storyId}.json`)
+    //     .then(({data}) => data && selectFields(data));
+    // return result;
+
     const result = await axios
-        .get(`${storyUrl}${storyId}.json`)
-        .then(({data}) => data && selectFields(data));
-    return result;
+        .get(`${storyUrl + storyId}.json`);
+
+    return selectFields(result.data);
 };
 
 export const getStoryIds = async () => {
-    const result = await axios.get(newStoriesUrl).then(({data}) => data);
-    return result;
+    // const result = await axios.get(newStoriesUrl).then(({data}) => data);
+    // return result;
+    const result = await axios.get(newStoriesUrl);
+
+    return result.data;
 };
